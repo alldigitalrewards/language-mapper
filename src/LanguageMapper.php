d21839ddcafd4f10dbd41fc83cdd36a03cdba784
jmuto <jmuto@alldigitalrewards.com>
2021-09-08 14:11:09 -0400

    AllowShortCodeAndLocalesWithDashes

--- src/LanguageMapper.php
+++ src/LanguageMapper.php
@@ -22,6 +22,20 @@ class LanguageMapper
         'ru_RU',
         'zh_Hans_CN',
     ];
+    const TWO_CHAR_LOCALE_MAPPER = [
+        'en' => 'en_US',
+        'es' => 'es_US',
+        'de' => 'de_DE',
+        'fr' => 'fr_CA',
+        'hi' => 'hi_IN',
+        'it' => 'it_IT',
+        'ja' => 'ja_JP',
+        'ko' => 'ko_KR',
+        'nl' => 'nl_NL',
+        'pt' => 'pt_BR',
+        'ru' => 'ru_RU',
+        'zh' => 'zh_Hans_CN',
+    ];
 
     /**
      * @var string
@@ -53,16 +67,29 @@ class LanguageMapper
             $this->language = self::DEFAULT_LANGUAGE;
             return;
         }
-        $language = trim($language);
         $this->language = $this->hydrateLanguage($language);
     }
 
     private function hydrateLanguage(string $language): string
     {
+        $language = $this->formatLanguageStringForHydration($language);
+
         if (in_array($language, self::ACCEPTABLE_LANGUAGES) === true) {
             return $language;
         }
 
+        foreach (self::TWO_CHAR_LOCALE_MAPPER as $shortCode => $longCode) {
+            if (strtolower($language) === $shortCode) {
+                return $longCode;
+            }
+        }
+
         return self::DEFAULT_LANGUAGE;
+    }
+
+    private function formatLanguageStringForHydration(string $language)
+    {
+        $language = trim($language);
+        return str_replace("-", "_", $language);
     }
 }

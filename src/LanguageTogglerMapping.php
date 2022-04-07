754546daca38e2d4b6cf879e260d237f47d9435f
Zechariah Walden <zech@zewadesign.com>
2022-04-07 10:01:29 -0500

    DS-3968 - add locales to languagetoggler mapping

--- src/LanguageTogglerMapping.php
+++ src/LanguageTogglerMapping.php
@@ -12,35 +12,25 @@ class LanguageTogglerMapping
     public function getMapping(): array
     {
         return [
-            'en_US' => [
-                'main' => 'English (US)',
-                'item' => 'English - US',
-                'flag' => $this->imageBasePath . '/en_US.png',
+            'pt_BR' => [
+                'main' => 'Portuguese (BR)',
+                'item' => 'Portuguese - BR',
+                'flag' => $this->imageBasePath . '/pt_BR.png',
             ],
-            'es_US' => [
-                'main' => 'Espanol (US)',
-                'item' => 'Espanol - US',
-                'flag' => $this->imageBasePath . '/en_US.png',
-            ],
-            'es_ES' => [
-                'main' => 'Espanol (ES)',
-                'item' => 'Espanol - ES',
-                'flag' => $this->imageBasePath . '/es_ES.png',
-            ],
-            'de_DE' => [
-                'main' => 'German (DE)',
-                'item' => 'German - DE',
-                'flag' => $this->imageBasePath . '/de_DE.png',
-            ],
-            'fr_CA' => [
-                'main' => 'French (CA)',
-                'item' => 'French - CA',
-                'flag' => $this->imageBasePath . '/fr_CA.png',
+            'en_GB' => [
+                'main' => 'English (UK)',
+                'item' => 'English - UK',
+                'flag' => $this->imageBasePath . '/en_GB.png',
             ],
             'fr_FR' => [
                 'main' => 'French (FR)',
                 'item' => 'French - FR',
                 'flag' => $this->imageBasePath . '/fr_FR.png',
+            ],
+            'de_DE' => [
+                'main' => 'German (DE)',
+                'item' => 'German - DE',
+                'flag' => $this->imageBasePath . '/de_DE.png',
             ],
             'hi_IN' => [
                 'main' => 'Hindu (IN)',
@@ -57,35 +47,65 @@ class LanguageTogglerMapping
                 'item' => 'Korean - KR',
                 'flag' => $this->imageBasePath . '/ko_KR.png',
             ],
-            'it_IT' => [
-                'main' => 'Italian (IT)',
-                'item' => 'Italian - IT',
-                'flag' => $this->imageBasePath . '/it_IT.png',
+            'zh_CN' => [
+                'main' => 'Chinese (CN)',
+                'item' => 'Chinese - CN',
+                'flag' => $this->imageBasePath . '/zh_CN.png',
             ],
-            'ms_MY' => [
-                'main' => 'Malaysian (MY)',
-                'item' => 'Malaysian - MY',
-                'flag' => $this->imageBasePath . '/ms_MY.png',
+            'zh_TW' => [
+                'main' => 'Chinese (TW)',
+                'item' => 'Chinese - TW',
+                'flag' => $this->imageBasePath . '/zh_TW.png',
             ],
             'nl_NL' => [
                 'main' => 'Dutch (NL)',
                 'item' => 'Dutch - NL',
                 'flag' => $this->imageBasePath . '/nl_NL.png',
             ],
-            'pt_BR' => [
-                'main' => 'Portuguese (BR)',
-                'item' => 'Portuguese - BR',
-                'flag' => $this->imageBasePath . '/pt_BR.png',
+            'nl_BE' => [
+                'main' => 'Dutch (BE)',
+                'item' => 'Dutch - BE',
+                'flag' => $this->imageBasePath . '/nl_BE.png',
+            ],
+            'ms_SG' => [
+                'main' => 'Malaysian (SG)',
+                'item' => 'Malaysian - SG',
+                'flag' => $this->imageBasePath . '/ms_SG.png',
+            ],
+            'ml_IN' => [
+                'main' => 'Malayalam (IN)',
+                'item' => 'Malayalam - IN',
+                'flag' => $this->imageBasePath . '/ml_IN.png',
+            ],
+            'fr_CA' => [
+                'main' => 'French (CA)',
+                'item' => 'French - CA',
+                'flag' => $this->imageBasePath . '/fr_CA.png',
             ],
             'ru_RU' => [
                 'main' => 'Russian (RU)',
                 'item' => 'Russian - RU',
                 'flag' => $this->imageBasePath . '/ru_RU.png',
             ],
-            'zh_Hans_CN' => [
-                'main' => 'Chinese (Hans_CN)',
-                'item' => 'Chinese - Hans_CN',
-                'flag' => $this->imageBasePath . '/zh_Hans_CN.png',
+            'es_ES' => [
+                'main' => 'Espanol (ES)',
+                'item' => 'Espanol - ES',
+                'flag' => $this->imageBasePath . '/es_ES.png',
+            ],
+            'it_IT' => [
+                'main' => 'Italian (IT)',
+                'item' => 'Italian - IT',
+                'flag' => $this->imageBasePath . '/it_IT.png',
+            ],
+            'es_US' => [
+                'main' => 'Espanol (US)',
+                'item' => 'Espanol - US',
+                'flag' => $this->imageBasePath . '/en_US.png',
+            ],
+            'en_US' => [
+                'main' => 'English (US)',
+                'item' => 'English - US',
+                'flag' => $this->imageBasePath . '/en_US.png',
             ],
         ];
     }
